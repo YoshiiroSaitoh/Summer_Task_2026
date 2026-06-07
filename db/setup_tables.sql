c7d3891f758c873f3201543b0834be54b36a2a98
Yosihiro Saitoh <shupeluter@hotmail.com>
2026-06-07 22:30:48 +0900

    SQL調整

--- db/setup_tables.sql
+++ db/setup_tables.sql
@@ -1,6 +1,12 @@
+﻿\connect summer
+
 CREATE TABLE IF NOT EXISTS temperature_logs (
     id BIGSERIAL PRIMARY KEY,
     probe_id VARCHAR(64) NOT NULL,
     recorded_at TIMESTAMPTZ NOT NULL,
     temperature NUMERIC(5, 2) NOT NULL
 );
+
+ALTER TABLE temperature_logs OWNER TO apluser;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE temperature_logs TO apluser;
+GRANT USAGE, SELECT, UPDATE ON SEQUENCE temperature_logs_id_seq TO apluser;
